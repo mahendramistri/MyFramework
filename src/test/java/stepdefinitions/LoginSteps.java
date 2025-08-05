1181ca18de2563519daeecacc181d0910355c897
Mahendra Mistri <mahendramistri@gmail.com>
2025-08-05 15:56:07 +0530

    added chrome 1

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -2,12 +2,14 @@ package stepdefinitions;
 
 import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LoginSteps {
     WebDriver driver;
     @Given("user is on login page")
     public void user_is_on_login_page() throws InterruptedException {
+        driver = new ChromeDriver();
         driver.get("https://chatgpt.com");
         Thread.sleep(3000);
         System.out.println("User navigates to login page");
